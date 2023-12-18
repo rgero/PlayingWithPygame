018a766bf95f9bcb1e09da1bebbdf4f2543c1964
Roy Gero <roy.gero.ii@gmail.com>
2023-12-18 12:40:18 -0500

    Completing the first section

--- Matcher/game.py
+++ Matcher/game.py
@@ -3,6 +3,12 @@ from pygame import display, event, image
 
 import game_config as gc
 from Animal import Animal
+from time import sleep
+
+def find_index(x,y):
+  row = y // gc.IMAGE_SIZE
+  col = x // gc.IMAGE_SIZE
+  return int(row * gc.NUM_TILES_PER_ROW + col)
 
 # This has to be first, it initializes everything
 pygame.init()
@@ -20,6 +26,7 @@ matched = image.load('other_assets/matched.png')
 currentlyRunning = True
 
 tiles = [Animal(i) for i in range(0, gc.NUM_TILES_TOTAL)]
+current_images=[]
 
 # Game Loop
 while currentlyRunning:
@@ -30,14 +37,41 @@ while currentlyRunning:
     if e.type is pygame.QUIT:
       currentlyRunning = False
 
+    # If the Escape key is pressed, close the game.
+    if e.type == pygame.KEYDOWN:
+      if e.key == pygame.K_ESCAPE:
+        currentlyRunning = False
+
+    if e.type == pygame.MOUSEBUTTONDOWN:
+      mouse_x, mouse_y = pygame.mouse.get_pos()
+      index = find_index(mouse_x, mouse_y)
+      if index not in current_images:
+        current_images.append(index)
+      if len(current_images) > 2:
+        current_images = current_images[1:]
+
   # Display the animal tiles
   screen.fill((250,250,250))
 
-  for tile in tiles:
-    targetX = tile.col * gc.IMAGE_SIZE + gc.MARGIN;
-    targetY = tile.row * gc.IMAGE_SIZE + gc.MARGIN;
-    screen.blit(tile.image, (targetX, targetY))
+  for _, tile in enumerate(tiles):
+    image_i = tile.image if tile.index in current_images else tile.box
+    if not tile.skip:
+      targetX = tile.col * gc.IMAGE_SIZE + gc.MARGIN;
+      targetY = tile.row * gc.IMAGE_SIZE + gc.MARGIN;
+      screen.blit(image_i, (targetX, targetY))
 
   display.flip()
+
+  # Check if they are the same
+  if len(current_images) == 2:
+    ix1, ix2 = current_images
+    if tiles[ix1].name == tiles[ix2].name:
+      tiles[ix1].skip = True
+      tiles[ix2].skip = True
+      sleep(0.6)
+      screen.blit(matched, (0,0))
+      display.flip()
+      sleep(0.4)
+      current_images = []
 
 print("Successfully Quit");
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 I'm just curious about PyGame and want to play around with it.
 
-Temporarily on pause due to how busy things have been on other projects
+I think this would be a quick way to make 2048.
